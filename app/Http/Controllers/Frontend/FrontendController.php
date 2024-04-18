bd82b0a893ab58ef76ee12ce688ebca7a7be4422
amandeepdhaliwals <amandeep@webmobsoft.com>
2024-04-18 11:18:21 +0530

    initia commit

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -13,7 +13,8 @@ class FrontendController extends Controller
      */
     public function index()
     {
-        return view('frontend.index');
+       // return view('frontend.index');
+        return redirect()->route('login');
     }
 
     /**
