a949e070e45545c896a6c933a6751d675d322aac
Rajinder kumar <rajinder@webmobsoft.com>
2024-04-22 19:02:45 +0530

    add export to excel functionality

---
+++ app/Exports/CustomerExport.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Exports;
+use App\Models\User;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+
+class CustomerExport implements FromCollection, WithHeadings
+{
+    protected $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function collection()
+    {
+        return $this->data;
+    }
+
+    public function headings(): array
+    {
+        // Define fixed headings here
+        return [
+            'Name',
+            'Email',
+            'Mobile',
+            'Store Name',
+            // Add more headings as needed
+        ];
+    }
+}
