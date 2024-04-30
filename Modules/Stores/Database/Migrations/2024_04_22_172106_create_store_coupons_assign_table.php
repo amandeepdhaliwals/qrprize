e0c2193e09b8c01c58547480b62705a8b14b94c8
Rajinder kumar <rajinder@webmobsoft.com>
2024-04-30 13:19:10 +0530

    add edit campaign functionality done

--- Modules/Stores/Database/Migrations/2024_04_22_172106_create_store_coupons_assign_table.php
+++ Modules/Stores/Database/Migrations/2024_04_22_172106_create_store_coupons_assign_table.php
@@ -13,12 +13,19 @@ return new class extends Migration
     {
         Schema::create('store_coupons_assign', function (Blueprint $table) {
             $table->id();
+            $table->string('campaign_name');
             $table->integer('user_id'); 
             $table->integer('coupon_id'); 
             $table->integer('total_no_of_coupons'); 
             $table->integer('no_of_winned_coupons'); 
             $table->string('qr_code_url')->nullable();
-            $table->string('qr_code_image')->nullable();
+            $table->longText('qr_code_image')->nullable();
+            $table->integer('adv_video_id');
+            $table->integer('primary_image_id');
+            $table->json('secondary_images_id');
+            $table->json('coupons_id');
+            $table->integer('lock_time');
+            $table->integer('winning_ratio');
             $table->integer('created_by')->unsigned()->nullable();
             $table->integer('updated_by')->unsigned()->nullable();
             $table->integer('deleted_by')->unsigned()->nullable();
