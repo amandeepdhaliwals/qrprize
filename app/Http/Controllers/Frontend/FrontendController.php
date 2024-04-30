4ca6610348e6c061b9afb15c4b2678ec284de894
Rajinder kumar <rajinder@webmobsoft.com>
2024-04-30 17:36:59 +0530

    did changes in frontend

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use Modules\Stores\Entities\Campaign; 
+use Modules\Advertisements\Entities\Advertisement; 
+use Modules\Coupons\Entities\Coupon; 
+use Illuminate\Http\Response;
 
 class FrontendController extends Controller
 {
@@ -37,8 +41,20 @@ class FrontendController extends Controller
         return view('frontend.terms');
     }
 
-    public function campaign()
+    public function campaign($storeId,$campaignId)
     {
-        return view('frontend.campaign');
+        $campaign = Campaign::where('id',$campaignId)->where('user_id',$storeId)->first();
+
+        if($campaign){
+            $advertisement_video = Advertisement::where('id',$campaign->adv_video_id)->select('media')->first();
+            $primary_image = Advertisement::where('id',$campaign->primary_image_id)->select('media','free_services')->first();
+            $secondary_images = Advertisement::whereIn('id',$campaign->secondary_images_id)->select('media','free_services')->get();
+            $lock_time = $campaign->lock_time;
+//dd($secondary_images);
+            return view('frontend.campaign',compact('advertisement_video','primary_image','secondary_images','lock_time'));
+
+        }else{
+            return abort(Response::HTTP_NOT_FOUND);
+        }
     }
 }
