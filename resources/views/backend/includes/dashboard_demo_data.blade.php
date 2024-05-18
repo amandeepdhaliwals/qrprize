bb844ef6ccf1caf08c818290c6be7c43ce2612cb
amandeepdhaliwals <amandeep@webmobsoft.com>
2024-05-18 13:37:24 +0530

    changes in dashboard

--- resources/views/backend/includes/dashboard_demo_data.blade.php
+++ resources/views/backend/includes/dashboard_demo_data.blade.php
@@ -1,4 +1,86 @@
 <div class="row">
+ <div class="col-6 col-lg-3">
+        <div class="card mb-4">
+            <div class="card-body p-3 d-flex align-items-center">
+                <div class="bg-warning text-white p-3 me-3">
+                   <i class="fa-solid fa-store"></i>
+                </div>
+                <div>
+                    <!-- <div class="fs-6 fw-semibold text-warning">$1.999,50</div> -->
+                    <div class="text-medium-emphasis text-uppercase fw-semibold small">Store</div>
+                </div>
+            </div>
+            <div class="card-footer px-3 py-2">
+                <a class="btn-block text-medium-emphasis d-flex justify-content-between align-items-center" href='{{ route("backend.stores.index") }}'><span class="small fw-semibold">View More</span>
+                    <i class="fa-solid fa-circle-chevron-right"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+        <!-- /.col-->
+        <div class="col-6 col-lg-3">
+        <div class="card mb-4">
+            <div class="card-body p-3 d-flex align-items-center">
+                <div class="bg-danger text-white p-3 me-3">
+                    <i class="fa-solid fa-user"></i>
+                </div>
+                <div>
+                    <!-- <div class="fs-6 fw-semibold text-danger">$1.999,50</div> -->
+                    <div class="text-medium-emphasis text-uppercase fw-semibold small">Users</div>
+                </div>
+            </div>
+            <div class="card-footer px-3 py-2">
+                <a class="btn-block text-medium-emphasis d-flex justify-content-between align-items-center" href='{{ route("backend.customers.index") }}'><span class="small fw-semibold">View More</span>
+                    <i class="fa-solid fa-circle-chevron-right"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+    <!-- /.col-->
+
+    <!-- /.col-->
+    <div class="col-6 col-lg-3">
+        <div class="card mb-4">
+            <div class="card-body p-3 d-flex align-items-center">
+                <div class="bg-info text-white p-3 me-3">
+                  <i class="fa-solid fa-tag"></i>
+                </div>
+                <div>
+                    <!-- <div class="fs-6 fw-semibold text-info">$1.999,50</div> -->
+                    <div class="text-medium-emphasis text-uppercase fw-semibold small">Winning Prizes</div>
+                </div>
+            </div>
+            <div class="card-footer px-3 py-2">
+                <a class="btn-block text-medium-emphasis d-flex justify-content-between align-items-center" href='{{ route("backend.coupons.index") }}'><span class="small fw-semibold">View More</span>
+                    <i class="fa-solid fa-circle-chevron-right"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+    <!-- /.col-->
+    <div class="col-6 col-lg-3">
+        <div class="card mb-4">
+            <div class="card-body p-3 d-flex align-items-center">
+                <div class="bg-primary text-white p-3 me-3">
+                 <i class="fa-solid fa-image"></i>
+                </div>
+                <div>
+                    <!-- <div class="fs-6 fw-semibold text-primary">10</div> -->
+                    <div class="text-medium-emphasis text-uppercase fw-semibold small">Images</div>
+                </div>
+            </div>                   
+            <div class="card-footer px-3 py-2">
+                <a class="btn-block text-medium-emphasis d-flex justify-content-between align-items-center" href='{{ route("backend.gallery.index") }}'><span class="small fw-semibold">View More</span>
+                    <i class="fa-solid fa-circle-chevron-right"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+<!-- /.row-->
+<div class="row">
     <div class="col-6 col-lg-3">
         <div class="card mb-4">
             <div class="card-body p-3 d-flex align-items-center">
@@ -22,58 +104,21 @@
         <div class="card mb-4">
             <div class="card-body p-3 d-flex align-items-center">
                 <div class="bg-info text-white p-3 me-3">
-                  <i class="fa-solid fa-tag"></i>
+                  <i class="fa-solid fa-image"></i>
                 </div>
                 <div>
                     <!-- <div class="fs-6 fw-semibold text-info">$1.999,50</div> -->
-                    <div class="text-medium-emphasis text-uppercase fw-semibold small">Coupons</div>
+                    <div class="text-medium-emphasis text-uppercase fw-semibold small">Other prizes Images</div>
                 </div>
             </div>
             <div class="card-footer px-3 py-2">
-                <a class="btn-block text-medium-emphasis d-flex justify-content-between align-items-center" href='{{ route("backend.coupons.index") }}'><span class="small fw-semibold">View More</span>
+                <a class="btn-block text-medium-emphasis d-flex justify-content-between align-items-center" href='{{ route("backend.otherprizes.index") }}'><span class="small fw-semibold">View More</span>
                     <i class="fa-solid fa-circle-chevron-right"></i>
                 </a>
             </div>
         </div>
     </div>
-    <!-- /.col-->
-    <div class="col-6 col-lg-3">
-        <div class="card mb-4">
-            <div class="card-body p-3 d-flex align-items-center">
-                <div class="bg-warning text-white p-3 me-3">
-                   <i class="fa-solid fa-store"></i>
-                </div>
-                <div>
-                    <!-- <div class="fs-6 fw-semibold text-warning">$1.999,50</div> -->
-                    <div class="text-medium-emphasis text-uppercase fw-semibold small">Store</div>
-                </div>
-            </div>
-            <div class="card-footer px-3 py-2">
-                <a class="btn-block text-medium-emphasis d-flex justify-content-between align-items-center" href=''><span class="small fw-semibold">View More</span>
-                    <i class="fa-solid fa-circle-chevron-right"></i>
-                </a>
-            </div>
-        </div>
-    </div>
-    <!-- /.col-->
-    <div class="col-6 col-lg-3">
-        <div class="card mb-4">
-            <div class="card-body p-3 d-flex align-items-center">
-                <div class="bg-danger text-white p-3 me-3">
-                    <i class="fa-solid fa-user"></i>
-                </div>
-                <div>
-                    <!-- <div class="fs-6 fw-semibold text-danger">$1.999,50</div> -->
-                    <div class="text-medium-emphasis text-uppercase fw-semibold small">Users</div>
-                </div>
-            </div>
-            <div class="card-footer px-3 py-2">
-                <a class="btn-block text-medium-emphasis d-flex justify-content-between align-items-center" href="#"><span class="small fw-semibold">View More</span>
-                    <i class="fa-solid fa-circle-chevron-right"></i>
-                </a>
-            </div>
-        </div>
-    </div>
-    <!-- /.col-->
+ 
+
 </div>
 <!-- /.row-->
