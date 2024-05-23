965b84deb72fca994178ad1003c16470c8ae30f5
Rajinder kumar <rajinder@webmobsoft.com>
2024-05-23 13:13:35 +0530

    changes revert

--- resources/views/frontend/campaign.blade.php
+++ resources/views/frontend/campaign.blade.php
@@ -1,29 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
   <title>
     page
   </title>
   <meta content="" name="description">
   <meta content="" name="keywords">
-  <meta name="permissions-policy" content="fullscreen=(), geolocation=()">
+
   <!-- Favicons -->
 <!--   <link href="" rel="icon">
   <link href="" rel="apple-touch-icon" -->
-
   <!-- Google Fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Inter:slnt,wght@-10..0,100..900&display=swap" rel="stylesheet">
-
   <!-- Vendor CSS Files -->
   <link href="{{ asset('assets/Impact/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
   <link href="{{ asset('assets/Impact/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <!-- Template Main CSS File -->
   <link href="{{ asset('assets/Impact/assets/css/main.css')}}" rel="stylesheet">
@@ -32,18 +27,13 @@
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <!-- <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
-
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap" rel="stylesheet">
 <!------ Include the above in your HEAD tag ---------->
-
 </head>
-
 <body>
-
   <main id="main">
     <!-- ======= Call To Action Section ======= -->
-
     <section id="call-to-action" class="call-to-action">
     <div class="banner-img">
       <div class="container text-center" data-aos="zoom-out">
@@ -54,26 +44,22 @@
         Your browser does not support the video tag.
       </video>
       @elseif($advertisement_video->media_type == "Youtube" || $advertisement_video->media_type == "youtube") 
-      <div id="player"></div>
-      <!-- <iframe id="youtube-player" width="100%" height="100%" src="{{ $advertisement_video->media }}" frameborder="0" allowfullscreen></iframe> -->
+      <iframe id="youtube-player" width="100%" height="100%" src="{{ $advertisement_video->media }}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       @elseif($advertisement_video->media_type == "Vimeo" || $advertisement_video->media_type == "vimeo") 
-      <iframe id="vimeo-player" src="{{ $advertisement_video->media }}" width="100%" height="100%" frameborder="0" allowfullscreen></iframe>
+      <iframe src="{{ $advertisement_video->media }}" width="100%" height="100%" frameborder="0" allowfullscreen></iframe>
       @endif
       </div>
        </div>
     </section>
  
     <!-- End Call To Action Section -->
-
    
 <section id="second-section" class="second-section">
   <div class="container" data-aos="zoom-out">
    <div class="section-header">
           <h4>{{$advertisement_detail->heading}}</h4>
         </div>
-
         <div class="row gx-lg-0 gy-4">
-
           <div class="col-lg-12 ">
           <div class="vacation-banner">
             <div class="itinerary-tag">
@@ -105,8 +91,6 @@
         </div>
   </div>
 </section>
-
-
 <section id="crousel">
     <div class="container">
     <div id="carouselExampleCaptions" class="carousel slide">
@@ -142,15 +126,12 @@
   </div>
   </div>
   </section>
-
     <!-- ======= prizes Section ======= -->
     <section id="prizes" class="prizes">
       <div class="container" data-aos="fade-up">
-
         <div class="section-header">
           <h4>{{$advertisement_detail->other_coupon_prize_heading}}</h4>
         </div>
-
         <div class="row gx-lg-0 gy-4">
           @foreach($other_images as $other_image)
           <div class="col-4">
@@ -165,7 +146,6 @@
       </div>
     </div>
     </section><!-- End Contact Section -->
-
     <section id="prizes" class="prizes">
     <div id="wheel">
             <div id="inner-wheel">
@@ -188,7 +168,6 @@
         
         <!-- <div id="txt"></div> -->
     </section><!-- End spin wheel Section -->
-
     <section class="spinner">
       <div class="container">
         <div class="row">
@@ -219,7 +198,6 @@
             <h4 class="mt-4 text-center">Better luck next time!</h4>
           </div>
         </div>
-
         <div class="row countdown-row">
           <div class="col-12">
             <div id="betterluckcountdown" class="betterluckcountdown"></div>
@@ -231,7 +209,6 @@
           </div>
         </div>
       </section>
-
     
       <section class="better-luck" style="display:none;">
             <div class="container">
@@ -262,7 +239,6 @@
      
         </div>
       </section>
-
       <section class="better-luck" style="display:none;">
             <div class="container">
         <div class="row">
@@ -273,7 +249,6 @@
             <br>
             <!-- <h2 class="mt-4 text-center">Congratulations!</h2> -->
             <p class="text-center">{{$coupons[0]->code}} <i class="far fa-copy"></i></p>
-
             <br>
             <p class="text-center">{{$coupons[0]->description}}</p>
           </div>
@@ -282,17 +257,13 @@
         <div class="col-12">
   
             <h2 class="mt-4 text-center">Terms and Conditions</h2>
-
             <br>
             <p class="text-center">{{$coupons[0]->terms_and_condition}}</p>
           </div>
      
         </div>
       </section>
-
   </main><!-- End #main -->
-
-
 <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog loginModal-dialog" role="document">
     <div class="modal-content">
@@ -335,8 +306,6 @@
     </div>
   </div>
 </div>
-
-
 <div class="modal fade" id="otpModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog loginModal-dialog" role="document">
     <div class="modal-content">
@@ -377,19 +346,13 @@ email & phone number to verify!</h4>
     </div>
   </div>
 </div>
-
   <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
   <div id="preloader"></div>
-
-
   <script>
 // Set the date we're counting down to
 var countDownDate = new Date("Jan 5, 2030 15:37:25").getTime();
-
 // Update the count down every 1 second
 var x = setInterval(function() {
-
   // Get today's date and time
   var now = new Date().getTime();
     
@@ -415,30 +378,26 @@ var x = setInterval(function() {
 
 
   <!-- Vendor JS Files -->
-  <script src="https://player.vimeo.com/api/player.js"></script>
 
+  <script src="https://www.youtube.com/iframe_api"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js" integrity="sha512-A7AYk1fGKX6S2SsHywmPkrnzTZHrgiVT7GcQkLGDe2ev0aWb8zejytzS8wjo7PGEXKqJOrjQ4oORtnimIRZBtw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <script src="{{ asset('assets/Impact/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
   <script src="{{ asset('assets/Impact/assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
   <script src="https://cdn.jsdelivr.net/npm/swiper@10.3.1/swiper.min.js"></script>
   <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.js"></script>
-
   <!-- Template Main JS File -->
   <script src="{{ asset('assets/Impact/assets/js/main.js') }}"></script>
   <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
 <script type="text/javascript">
    //set default degree (360*5)
 var degree = 1800;
 //number of clicks = 0
 var clicks = 0;
-
 $(document).ready(function(){
 	
 	/*WHEEL SPIN FUNCTION*/
@@ -461,11 +420,9 @@ $(document).ready(function(){
     //  $('#inner-wheel').one('transitionend', function() {
     //         $('#loginModal').modal('show');
     //     });
-
     setTimeout(function() {
             $('#loginModal').modal('show');
         }, 2000); 
-
 		$('#wheel .sec').each(function(){
 			var t = $(this);
 			var noY = 0;
@@ -508,10 +465,7 @@ $(document).ready(function(){
 	
 	
 });//DOCUMENT READY
-
-
   </script>
-
 <script>
     document.addEventListener("DOMContentLoaded", function() {
       function enableSpinner() {
@@ -519,11 +473,9 @@ $(document).ready(function(){
         }
         // Get the video element
         var video = document.getElementById("video-player");
-
         if(video){
           // Get the video container
           var videoContainer = document.getElementById("video-container");
-
           // Function to handle scroll event
           function handleScroll() {
             var position = video.getBoundingClientRect();
@@ -532,17 +484,14 @@ $(document).ready(function(){
                 video.pause();
               }
           }
-
           // Listen for scroll event
           window.addEventListener("scroll", handleScroll);
-
           // Listen for video timeupdate event to get watched time
           video.addEventListener("timeupdate", function() {
               // Get the current time in seconds
               var watchedTime = Math.floor(video.currentTime);
               var timetext = "Watched time: " + watchedTime + " seconds";
               //document.getElementById("watched-time").textContent = timetext
-
           });
           video.addEventListener("play", function() {
               console.log("Video started playing.");
@@ -550,11 +499,9 @@ $(document).ready(function(){
               //document.getElementById("text_to_spin").style.display = "none";
               // You can add additional actions here when the video starts playing
           });
-
           video.addEventListener("ended", function() {
             console.log("Video ended.");
             enableSpinner();
-
             var watchedTime = Math.floor(video.currentTime);
             var timetext = "Watched time: " + watchedTime + " seconds";
             //document.getElementById("watched-time").textContent = timetext
@@ -563,37 +510,8 @@ $(document).ready(function(){
             // You can also display a message or trigger another action.
           });
         }
-        var vimeo = document.getElementById('vimeo-player');
-        if (vimeo) {
-            var player = new Vimeo.Player(vimeo);
-
-            // Listen for the 'play' event
-            player.on('play', function() {
-                console.log('The video started playing');
-            });
-
-            // Listen for the 'ended' event
-            player.on('ended', function() {
-                console.log('The video has ended');
-                enableSpinner();
-                var watchedTime = Math.floor(video.currentTime);
-                var timetext = "Watched time: " + watchedTime + " seconds";
-            });
-
-            function handleScroll() {
-            var position = vimeo.getBoundingClientRect();
-              // Pause the video if it's in the viewport
-              if (position.top <= -150 && position.bottom <= window.innerHeight) {
-                player.pause();
-              }
-          }
-
-          // Listen for scroll event
-          window.addEventListener("scroll", handleScroll);
-        } 
-      });
-    
-</script>
+    });
+    </script>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script>
@@ -606,7 +524,6 @@ $(document).ready(function(){
     $('#create_user').click(function(e){
       e.preventDefault(); 
     
-
       if($('input[name="first_name"]').val() == ''){
          $('#first_name_error').text('Please enter first name.');
             // $('html, body').animate({
@@ -615,7 +532,6 @@ $(document).ready(function(){
             setTimeout(function() {
                 $('#first_name_error').empty();
             }, 3000);
-
       }else if($('input[name="last_name"]').val() == ''){
          $('#last_name_error').text('Please enter last name.');
             // $('html, body').animate({
@@ -624,7 +540,6 @@ $(document).ready(function(){
             setTimeout(function() {
                 $('#last_name_error').empty();
             }, 3000);
-
       }else if($('input[name="email"]').val() == ''){
          $('#email_error').text('Please enter email.');
             // $('html, body').animate({
@@ -641,7 +556,6 @@ $(document).ready(function(){
             setTimeout(function() {
                 $('#phone_number_error').empty();
             }, 3000);
-
       }else{
          var customData = {
             store_id: $('input[name="store_id"]').val(),
@@ -666,12 +580,10 @@ $(document).ready(function(){
                 if(response.response_type == 'success'){
                   $('#loginModal').modal('hide');
                   $('#otpModal').modal('show');
-
                   document.querySelector('input[name="store_id_otp"]').value = response.storeId;
                   document.querySelector('input[name="campaign_id_otp"]').value = response.campaign_id;
                   document.querySelector('input[name="advertisement_id_otp"]').value = response.adverisement_id;
                   document.querySelector('input[name="user_id_otp"]').value = response.user_id;
-
                   document.getElementById("otp_email").textContent = 'Dummy OTP For Email: '+response.email_otp;
                   document.getElementById("otp_mobile").textContent = 'Dummy OTP For Mobile: '+response.mobile_otp;
                 }else{
@@ -719,13 +631,10 @@ $(document).ready(function(){
          });
       }
    });
-
-
    ////////////////////////OTP Verfication//////////////////////////////
    $('#otp_verification').click(function(e){
       e.preventDefault(); 
     
-
       if($('input[name="email_otp"]').val() == ''){
          $('#email_otp_error').text('Please enter email OTP.');
             // $('html, body').animate({
@@ -734,7 +643,6 @@ $(document).ready(function(){
             setTimeout(function() {
                 $('#email_otp_error').empty();
             }, 3000);
-
       }else if($('input[name="phone_number_otp"]').val() == ''){
          $('#phone_number_otp_error').text('Please enter phone number OTP.');
             // $('html, body').animate({
@@ -743,7 +651,6 @@ $(document).ready(function(){
             setTimeout(function() {
                 $('#phone_number_otp_error').empty();
             }, 3000);
-
       }else{
          var customData = {
             store_id: $('input[name="store_id_otp"]').val(),
@@ -792,78 +699,6 @@ $(document).ready(function(){
       }
    });
 });
-
 </script>
-<script>
-     var advertisementVideo = {
-        media_type: "<?php echo addslashes($advertisement_video->media_type); ?>"
-    };
-  if(advertisementVideo.media_type == "Youtube" || advertisementVideo.media_type == "youtube"){ 
-      function getYouTubeVideoId(url) {
-              const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
-              const matches = url.match(regex);
-              return matches ? matches[1] : null;
-      }
-
-      var videoUrl = "{{ $advertisement_video->media }}"; // Static video URL for testing
-      var videoId = getYouTubeVideoId(videoUrl);
-      console.log("Extracted Video ID:", videoId);
-      if(videoId){
-        var tag = document.createElement('script');
-        tag.src = "https://www.youtube.com/iframe_api";
-        var firstScriptTag = document.getElementsByTagName('script')[0];
-        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-        var player;
-        function onYouTubeIframeAPIReady() {
-          player = new YT.Player('player', {
-            height: '100%',
-            width: '100%',
-            videoId: videoId,
-            events: {
-              'onReady': onPlayerReady,
-              'onStateChange': onPlayerStateChange
-            }
-          });
-        }
-
-        function onPlayerReady(event) {
-          console.log('ready')
-          event.target.playVideo();
-        }
-
-        function onPlayerStateChange(event) {
-                  console.log('Player state changed:', event.data); // Log the state change
-                if (event.data == YT.PlayerState.PLAYING) {
-                    console.log('Video is playing');
-                } else if (event.data == YT.PlayerState.ENDED) {
-                    console.log('Video has ended');
-                    document.getElementById("spinner-overlay").style.display = "none";
-                }
-        }
-       
-        function pauseVideo() {
-          if (player) {
-            player.pauseVideo();
-          }
-        }
-
-        function NotPlayerInViewport() {
-          const rect = document.getElementById('player').getBoundingClientRect();
-          console.log(rect.top);
-          return (
-            rect.top <= -150 && rect.bottom <= window.innerHeight
-          );
-        }
-
-        window.addEventListener('scroll', function() {
-          if (NotPlayerInViewport()) {
-            pauseVideo();
-          }
-        });
-      }
-    }
-  </script>
 </body>
-
 </html>
