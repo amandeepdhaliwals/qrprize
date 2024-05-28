1a4356552816afd65499d9a3542124f39057e797
Rajinder kumar <rajinder@webmobsoft.com>
2024-05-28 18:16:02 +0530

    filter added

--- Modules/Customers/Http/Middleware/GenerateMenus.php
+++ Modules/Customers/Http/Middleware/GenerateMenus.php
@@ -12,21 +12,21 @@ class GenerateMenus
      */
     public function handle(Request $request, Closure $next)
     {
-        \Menu::make('admin_sidebar', function ($menu) {
-            // Customers
-            $menu->add('<i class="nav-icon fa-solid fa-ticket"></i> '.__('Customers'), [
-                'route' => 'backend.customers.index',
-                'class' => 'nav-item',
-            ])
-                ->data([
-                    'order' => 100, // Change the order if necessary
-                    'activematches' => ['admin/customers*'], // Adjust the URL pattern if necessary
-                    'permission' => ['view_customers'], // Add necessary permissions
-                ])
-                ->link->attr([
-                    'class' => 'nav-link',
-                ]);
-        })->sortBy('order');
+        // \Menu::make('admin_sidebar', function ($menu) {
+        //     // Customers
+        //     $menu->add('<i class="nav-icon fa-solid fa-ticket"></i> '.__('Customers'), [
+        //         'route' => 'backend.customers.index',
+        //         'class' => 'nav-item',
+        //     ])
+        //         ->data([
+        //             'order' => 100, // Change the order if necessary
+        //             'activematches' => ['admin/customers*'], // Adjust the URL pattern if necessary
+        //             'permission' => ['view_customers'], // Add necessary permissions
+        //         ])
+        //         ->link->attr([
+        //             'class' => 'nav-link',
+        //         ]);
+        // })->sortBy('order');
 
         return $next($request);
     }
--- Modules/Customers/Resources/views/backend/customers/stats.blade.php
+++ Modules/Customers/Resources/views/backend/customers/stats.blade.php
@@ -13,7 +13,72 @@
 
 @section('content')
 <!-- <x-backend.layouts.create :module_name="$module_name" :module_path="$module_path" :module_title="$module_title" :module_icon="$module_icon" :module_action="$module_action" /> -->
-
+<form action='{{ route("backend.customers.stats.filter") }}' method="POST">
+<div class="row mb-3">
+              @csrf
+<div class="col-12 col-sm-3">
+    <div class="form-group">
+        <?php
+        $field_name = 'year'; // Assuming the field name is 'year'
+        $field_label = ''; // Assuming the label for the field is 'Year'
+        $field_placeholder = "-- Select a year --";
+        $required = "";
+        $start_year = date('Y');
+        $end_year = date('Y') - 10;
+        $select_options = [];
+        
+        for ($year = $start_year; $year >= $end_year; $year--) {
+            $select_options[$year] = $year;
+        }
+        if($selectedYear == true){
+            $year_selected = $selected_year;
+        }else{
+            $year_selected = '';
+        }
+        ?>
+        
+        {{ html()->label($field_label, $field_name)->class('form-label') }} {!! $required !!}
+        {{ html()->select($field_name, $select_options, $year_selected)->placeholder($field_placeholder)->class('form-select')->attributes(["$required"]) }}
+    </div>
+    </div>
+    @if(auth()->check() && auth()->user()->roles->first()->id === 1)
+    <div class="col-12 col-sm-3">
+    <div class="form-group">
+        <?php
+        $field_name = 'store'; // Assuming the field name is 'year'
+        $field_label = ''; // Assuming the label for the field is 'Year'
+        $field_placeholder = "-- Select a store --";
+        $required = "";
+        $select_options = [];
+        
+        foreach ($stores as $store) {
+            $select_options[$store->id] = $store->name;
+        }
+        if($store_id != null){
+            $selected_store = $store_id;
+        }else{
+            $selected_store = '';
+        }
+        ?>
+        
+        {{ html()->label($field_label, $field_name)->class('form-label') }} {!! $required !!}
+        {{ html()->select($field_name, $select_options, $selected_store)->placeholder($field_placeholder)->class('form-select')->attributes(["$required"]) }}
+    
+    </div>
+    </div>
+    @endif
+    <div class="col-12 col-sm-1">
+    <div class="form-group">
+        <button type="submit" class="btn btn-primary" style="margin-top:10px;">Filter</button>
+    </div>
+    </div>
+    <div class="col-12 col-sm-1">
+    <div class="form-group">
+        <a href="{{ route('backend.customers.stats') }}"><button type="button" class="btn btn-primary" style="margin-top:10px;">Reset</button></a>
+    </div>
+    </div>
+</div>
+</form>
 <div class="container">
         {!! $chart->container() !!}
     </div>
