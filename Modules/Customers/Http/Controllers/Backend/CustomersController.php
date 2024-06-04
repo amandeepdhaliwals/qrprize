dd9abfc586ad1421c62955b5a0af4d735f57f4eb
amandeepdhaliwals <amandeep@webmobsoft.com>
2024-06-04 11:37:43 +0530

    changes in customer controller

--- Modules/Customers/Http/Controllers/Backend/CustomersController.php
+++ Modules/Customers/Http/Controllers/Backend/CustomersController.php
@@ -316,6 +316,7 @@ class CustomersController extends BackendBaseController
             }
             $totalVisitors = $totalVisitors->value('count'); // Use value() to directly get the count
 
+
             $totalRegistered = User::selectRaw('COUNT(DISTINCT id) as count')
                 ->when($store_id, function ($query) use ($store_id) {
                     $query->where('users.store_id', $store_id);
@@ -324,6 +325,10 @@ class CustomersController extends BackendBaseController
             if ($request->isMethod('post')) {
                 $totalRegistered->whereYear('users.created_at', $year);
             }
+
+            $totalRegistered->whereHas('roles', function ($query) use ($rolesId) {
+                $query->whereIn('roles.id', $rolesId);
+            });
 
             $totalRegistered = $totalRegistered->value('count'); // Use value() to directly get the count
 
@@ -380,11 +385,9 @@ class CustomersController extends BackendBaseController
 
             $totalVisitors = Visitor::selectRaw('COUNT(DISTINCT user_id_cookie) as count')
             ->where('visitors.store_id', '=' , $login_user_id);
-        
             if ($request->isMethod('post')) {
                 $totalVisitors->whereYear('visitors.created_at', $year);
             }
-            
             $totalVisitors = $totalVisitors->value('count'); // Use value() to directly get the count
             
 
@@ -394,7 +397,9 @@ class CustomersController extends BackendBaseController
             if ($request->isMethod('post')) {
                 $totalRegistered->whereYear('users.created_at', $year);
             }
-
+            $totalRegistered->whereHas('roles', function ($query) use ($rolesId) {
+                $query->whereIn('roles.id', $rolesId);
+            });
             $totalRegistered = $totalRegistered->value('count'); // Use value() to directly get the count
       
          }
